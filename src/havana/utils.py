458db5585d3b01c64fd6aebfeef65fbe12ffea00
Shashank B R <shabr@ebi.ac.uk>
2022-05-16 10:51:09 +0100

    db conf changes

--- src/havana/utils.py
+++ src/havana/utils.py
@@ -13,13 +13,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import Any
 
-from pymysql.connections import Connection
-from pymysql.cursors import Cursor
-
-
-def query(sql: str, cursor: Cursor, conn: Connection) -> tuple[tuple[Any, ...], ...]:
+def query(sql, cursor, conn):
     """
     Fetch all rows for a given SQL query.
 
